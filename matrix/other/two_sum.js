1ce311d0fc9def6c0396bc6ee39ffe8927639091
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2020-02-07 00:00:11 -0500

    twosum

---
+++ matrix/other/two_sum.js
@@ -0,0 +1,9 @@
+var twoSum = function(arr, target) {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === target){
+                return [i, j]
+            }
+        }
+    }
+};
